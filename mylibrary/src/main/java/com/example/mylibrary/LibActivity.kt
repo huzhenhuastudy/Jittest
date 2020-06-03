87036f6168f069f14a69a2c106750437fb8ac6c0
huzhenhua <799305676@qq.com>
2020-06-03 15:34:15 +0800

    v3

--- mylibrary/src/main/java/com/example/mylibrary/LibActivity.kt
+++ mylibrary/src/main/java/com/example/mylibrary/LibActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_lib.*
+import udesk.core.utils.UdeskUtils
 
 class LibActivity : AppCompatActivity() {
 
@@ -11,5 +12,6 @@ class LibActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lib)
         Glide.with(this).load(R.mipmap.video_play).into(lib_img)
+        test.text = UdeskUtils.getAppName(this)
     }
 }
