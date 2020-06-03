1090b420bb17996a230545dd5334febc01a95d5e
huzhenhua <799305676@qq.com>
2020-06-03 14:02:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jittest
+ 
---
+++ mylibrary/src/main/java/com/example/mylibrary/LibActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mylibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lib)
+    }
+}
