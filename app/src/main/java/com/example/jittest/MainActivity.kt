b6f7b40cfa536154599149b16cb4301ff69ac2f0
huzhenhua <799305676@qq.com>
2020-06-03 19:04:27 +0800

    修改

--- app/src/main/java/com/example/jittest/MainActivity.kt
+++ app/src/main/java/com/example/jittest/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.jittest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mylibrary.LibActivity
 //import com.bumptech.glide.Glide
-import com.example.mylibrary2.LibActivity2
+//import com.example.mylibrary2.LibActivity2
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_button.setOnClickListener { startActivity(Intent(this, LibActivity2::class.java)) }
+        tv_button.setOnClickListener { startActivity(Intent(this, LibActivity::class.java)) }
 //        Glide.with(this).load("http://pro-cs.kefutoutiao.com/sdk_upload/tid3055/a05a85e9efc65f298b818206eeca8c94_upload.jpg?x-oss-process=image/auto-orient,1/resize,h_300,w_300").into(img)
     }
 }
