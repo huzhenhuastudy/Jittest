0c1c7b48bf5a52783fabcb85392c4cbd014eb331
huzhenhua <799305676@qq.com>
2020-06-03 15:12:40 +0800

    升级

--- mylibrary/src/main/java/com/example/mylibrary/LibActivity.kt
+++ mylibrary/src/main/java/com/example/mylibrary/LibActivity.kt
@@ -2,11 +2,14 @@ package com.example.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_lib.*
 
 class LibActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lib)
+        Glide.with(this).load(R.mipmap.video_play).into(lib_img)
     }
 }
