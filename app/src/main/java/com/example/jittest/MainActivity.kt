d55d62e8f5f82fba8ef43d1ab7be26283beb4298
huzhenhua <799305676@qq.com>
2020-06-06 10:45:18 +0800

    修改配置4

--- app/src/main/java/com/example/jittest/MainActivity.kt
+++ app/src/main/java/com/example/jittest/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.jittest
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mylibrary.LibActivity
-//import com.bumptech.glide.Glide
-//import com.example.mylibrary2.LibActivity2
 import kotlinx.android.synthetic.main.activity_main.*
+import udesk.core.utils.UdeskUtils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_button.setOnClickListener { startActivity(Intent(this, LibActivity::class.java)) }
-//        Glide.with(this).load("http://pro-cs.kefutoutiao.com/sdk_upload/tid3055/a05a85e9efc65f298b818206eeca8c94_upload.jpg?x-oss-process=image/auto-orient,1/resize,h_300,w_300").into(img)
+
+        tv_button.text = UdeskUtils.getAppName(this)
     }
 }
